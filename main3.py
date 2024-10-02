64398d7392202ea1741c7600d2819f6a8d81b97f
nahyunlee <awsed2486@gmail.com>
2024-10-02 08:22:31 +0000

    복잡한 조건문 대신 상태변수 사용

--- main3.py
+++ main3.py
@@ -34,67 +34,48 @@ if __name__ == "__main__":
         with st.chat_message("ai" if content['role'] == "model" else "user"):
             st.markdown(content['text'])
 
-    # 시험 내용을 입력했는지 안했는지?
-    if 'question_exist' not in st.session_state:
-        st.session_state.question_exist = False
-
-    # 첫 질문인지 아닌지?
-    if 'first_quiz' not in st.session_state:
-        st.session_state.first_quiz = True
-
-    # 유저가 답변을 해야되는 상황인지 아닌지?
-    if 'user_answer_waiting' not in st.session_state:
-        st.session_state.user_answer_waiting = True
-
-    # 모델이 답변을 해야하는 상황인지 아닌지?
-    if 'generating_question' not in st.session_state:
-        st.session_state.generating_question = False
-
-    # 모델이 피드백을 해야하는 상황인지 아닌지?
-    if 'generating_feedback' not in st.session_state:
-        st.session_state.generating_feedback = False
+    # 상태 기록 변수
+    if 'status' not in st.session_state:
+        st.session_state.status = 0
 
 
-    # user : 시험 내용 입력 
-    if st.session_state.first_quiz == True:
+    # user : 시험 내용 입력
+    if st.session_state.status == 0:
         if "exam_content" not in st.session_state:
             st.session_state["exam_content"] = None
         if st.session_state["exam_content"] is None: # 시험 내용 입력을 기다림
             st.session_state["exam_content"] = st.chat_input("시험 내용을 입력하세요.")
         if st.session_state["exam_content"]: # 사용자가 입력을 하면 다음 단계로 진행
-            st.session_state.question_exist = True
-    else:
-        st.session_state.generating_question = True
+            st.session_state["chat_session"].append({"role":"user", "text":st.session_state["exam_content"]})
+            st.session_state.status = 1
+    
 
     # ai : 질문 생성
-    if (st.session_state.question_exist and st.session_state.first_quiz) or st.session_state.generating_question:
+    if st.session_state.status == 1:
         with st.chat_message("ai"):
             message_placeholder = st.empty() # DeltaGenerator 반환
             with st.spinner("질문 생성 중입니다."):
                 quiz = chatbot.ask_question(st.session_state["exam_content"])
                 st.markdown(quiz)
             st.session_state["chat_session"].append({"role":"ai", "text":quiz})
-        st.session_state.generating_question = False
-        st.session_state.first_quiz = False
-        st.session_state.user_answer_waiting = True
+        st.session_state.status = 2
     
 
     # user : 답변 입력
-    if st.session_state.first_quiz == False and st.session_state.question_exist and st.session_state.user_answer_waiting:
+    if st.session_state.status == 2:
         if "answer" not in st.session_state:
             st.session_state["answer"] = None
         if st.session_state["answer"] is None: # 메시지 입력을 기다림
-            st.session_state["answer"] = st.chat_input("메시지를 입력하세요.")
+            st.session_state["answer"] = st.chat_input("문제에 답하시오.")
         if st.session_state["answer"]: # 사용자가 입력을 하면 다음 단계로 진행
-            st.session_state.generating_feedback = True
             with st.chat_message("user"):
                 st.markdown(st.session_state["answer"])
             st.session_state["chat_session"].append({"role": "user", "text": st.session_state["answer"]})
-            st.session_state.user_answer_waiting = False
+            st.session_state.status = 3
         
 
     # ai : 피드백 생성
-    if st.session_state.generating_feedback:
+    if st.session_state.status == 3:
         with st.chat_message("ai"):
             message_placeholder = st.empty() # DeltaGenerator 반환
             with st.spinner("피드백 생성 중입니다."):
@@ -102,6 +83,6 @@ if __name__ == "__main__":
                 st.markdown(feedback)
             st.session_state["chat_session"].append({"role":"ai", "text":feedback})
             st.session_state["answer"] = None
-        st.session_state.generating_feedback = False
-        if st.button("문제 생성", disabled=st.session_state.generating_question):
+        st.session_state.status = 4
+        if st.button("문제 생성"):
             pass
